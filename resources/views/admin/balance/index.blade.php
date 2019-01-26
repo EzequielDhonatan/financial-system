d6b31b415d4a746a4074400b6da36afddc6b6045
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-01-26 18:47:31 -0200

    Layout Exibição do Saldo

---
+++ resources/views/admin/balance/index.blade.php
@@ -0,0 +1,50 @@
+@extends('adminlte::page')
+
+@section('title', 'Saldo')
+
+@section('content_header')
+    <h1>Saldo</h1>
+
+    <div class="breadcrumb">
+        <li><a href="">Dashboard</a></li>
+        <li><a href="">Saldo</a></li>
+    </div>
+@stop
+
+@section('content')
+    <div class="box">
+
+        <div class="box-header">
+                
+            <a href="" class="btn btn-primary">
+                <i class="fa fa-cart-plus" aria-hidden="true">
+                        Recarregar
+                </i>
+            </a>
+
+            <a href="" class="btn btn-danger">
+                <i class="fa fa-cart-plus" aria-hidden="true">
+                    Sacar
+                </i>
+            </a>
+
+        </div> <!-- box-header -->
+
+        <div class="box-body">
+            <div class="small-box bg-green">
+
+                <div class="inner">
+                    <h3>R$90,00</h3>
+                </div>
+
+                <div class="icon">
+                    <i class="ion ion-cash"></i>
+                </div>
+
+                <a href="#" class="small-box-footer">Histórico<i class="fa fa-arrow-circle-right"></i></a>
+
+            </div> <!-- small-box bg-green -->
+        </div> <!-- box-body -->
+
+    </div> <!-- box -->
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-$this->group(['middleware' => ['auth'], 'namespace' => 'Admin'], function() {
-    $this->get('admin', 'AdminController@index')->name('admin.home'); # ADMIN
+$this->group(['middleware' => ['auth'], 'namespace' => 'Admin', 'prefix' => 'admin'], function() {
+
+    $this->get('balance', 'BalanceController@index')->name('admin.balance'); # BALANCE
+
+    $this->get('/', 'AdminController@index')->name('admin.home'); # ADMIN
+
 });
 
 $this->get('/', 'Site\SiteController@index')->name('home'); # HOME
