5a761066318fc83d2902aebaa71568034214ec68
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-01-29 19:10:22 -0200

    Registro de Históricos de Recargas

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -12,10 +12,19 @@ class Balance extends Model
 
     public function deposit(float $value) : Array
     {
+        $totalBefore = $this->amount ? $this->amount : 0;
         $this->amount += number_format($value, 2, '.', '');
         $deposit = $this->save();
 
-        if ($deposit)
+        $historic = auth()->user()->historics()->create([
+            'type'                      => 'I',
+            'amount'                    => $value,
+            'total_before'              => $totalBefore,
+            'total_after'               => $this->amount,
+            'date'                      => date('Ymd'),
+        ]);
+
+        if ($deposit && $historic)
             return [
                 'success' => true,
                 'message' => 'Sucesso ao recarregar'
--- app/Models/Historic.php
+++ app/Models/Historic.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Historic extends Model
 {
-    //
+    protected $fillable = [
+        'type', 'amount', 'total_before', 'total_after', 'user_id_transaction', 'date'
+    ];
 }
